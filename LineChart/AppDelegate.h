75d363db40eb8bdd3fde476848fbfd0a0ab3f9e5
hpsoar <hpsoar@gmail.com>
2014-09-09 10:19:47 +0800

    Initial Commit

---
+++ LineChart/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LineChart
+//
+//  Created by HuangPeng on 9/9/14.
+//  Copyright (c) 2014 Beacon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LineChart/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LineChart
+//
+//  Created by HuangPeng on 9/9/14.
+//  Copyright (c) 2014 Beacon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
